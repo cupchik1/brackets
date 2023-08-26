15cd2261f4adf71279f20424921b02b5d06347ec
Alina Bezuhla <alinabezuglaya123@gmail.com>
2023-08-26 14:30:59 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  openBracketIndex = 0;
+  closeBracketIndex = 0;
+  result = false;
+  openBrackets = [];
+  closeBrackets = [];
+  buffer = [];
+
+  for(let i = 0; i < bracketsConfig.length; i++) {
+      openBrackets.push(bracketsConfig[i][0]);
+      closeBrackets.push(bracketsConfig[i][1]);
+  }
+    str = str.split('');
+    for (let i = 0; i < str.length; i++) {
+      openBracketIndex = openBrackets.indexOf(str[i]);
+      if (openBracketIndex !== -1) {
+        buffer.push(openBracketIndex);
+        continue;
+      }
+      closeBracketIndex = closeBrackets.indexOf(str[i]);
+      if (closeBracketIndex !== -1) {
+        openBracketIndex = buffer.pop();
+        if (closeBracketIndex !== openBracketIndex) {
+          return result;
+        }
+      }
+    }
+    if (buffer.length !== 0) {
+      return result;
+    }
+    
+    result = true;
+    return result;
 }
